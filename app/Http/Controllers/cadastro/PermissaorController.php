b4acf8f8f9fdcbbfe1b30307f09c2db0a30dec94
Adriano <rrjbarbosa@gmail.com>
2025-10-01 19:56:31 -0300

    editando permissaoPorAcessoSalvar() de PremissaorContreller

--- app/Http/Controllers/cadastro/PermissaorController.php
+++ app/Http/Controllers/cadastro/PermissaorController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\cadastro;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\permissao\permissoesParaAcessoRequest;
+use App\Models\cadastro\AcessorPermissaor;
 use App\Models\cadastro\Modulor;
 use Illuminate\Http\Request;
 use App\Models\cadastro\Permissaor;
 use App\Models\ErrorLog;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Microsoft\Graph\Generated\Models\Permission;
+use Illuminate\Support\Str;
 
 class PermissaorController extends Controller
 {
@@ -26,24 +30,31 @@ class PermissaorController extends Controller
     }
     
     public function permissaoPorAcessoSalvar(Request $request){
+        sleep(3);
         $user = $request->user();
         try{
             DB::transaction(function () use ($request, $user) {
-                $user_id    = $request->user_id; 
+                $acesso_id    = $request->acesso_id; 
                 $grupo      = $request->user()->grupo_empresar_id;   
                
-                AcessorUser::where('user_id', $request->user_id)->delete();
+                //AcessorPermissaor::where('user_id', $request->user_id)->delete();
+
+                $permissoesModulos = Permissaor::select('id', 'modulor_id')->get();
+                $permissoesModulos = json_decode($permissoesModulos); // O 'true' converte para array associativo
+
                 
-                $dados = collect($request->acessos)->map(function($acessorId) use ($user_id, $grupo) {
+                $dados = collect($request->acessos)->map(function($permissaoId) use ($acesso_id, $grupo, $permissoesModulos) {
                     return [
                         'id'                => Str::uuid()->toString(),
-                        'user_id'           => $user_id,
-                        'acessor_id'        => $acessorId,
+                        'acessor_id'        => $acesso_id,
+                        'permissaor_id'     => $permissaoId,
+                        'modulor_id'        => collect($permissoesModulos)->where('id', $permissaoId)->pluck('modulor_id')->first(),
                         'grupo_empresar_id' => $grupo,
                     ];
                 })->toArray();
 
-                AcessorUser::insert($dados);
+                /*AcessorPermissaor::insert($dados);*/
+                Log::info($permissoesModulos);
             });
             $permissoes = Permissaor::permissoesPorUsuario($request->user_id);
               
