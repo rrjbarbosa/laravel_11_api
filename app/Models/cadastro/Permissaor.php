d3bc124f055f8e1f3b16e9a472c2915f7382a57c
Adriano <interseadev02@gmail.com>
2025-10-06 21:40:31 -0300

    Retirado exibição de permissão duplicada se a mesma estivesse atribuida a dois acesso diferetes

--- app/Models/cadastro/Permissaor.php
+++ app/Models/cadastro/Permissaor.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Support\Facades\DB;
 
-
 class Permissaor extends Model
 {
     use HasFactory, HasUuids;
@@ -15,7 +14,7 @@ class Permissaor extends Model
     protected $fillable = ['id', 'nome', 'nome_exibicao', 'modulor_id'];
 
     static function permissoesPorUsuario($user_id) {
-        $permissoes = DB::table('permissaors')
+        return DB::table('permissaors')
             ->leftJoin('acessor_permissaor', 'permissaors.id', '=', 'acessor_permissaor.permissaor_id')
             ->leftJoin('acessors', 'acessor_permissaor.acessor_id', '=', 'acessors.id')
             ->leftJoin('acessor_user', function($join) use ($user_id) {
@@ -26,30 +25,29 @@ class Permissaor extends Model
                 'permissaors.id',
                 'permissaors.nome',
                 'permissaors.nome_exibicao',
-                DB::raw('IF(acessor_user.user_id IS NULL, 0, 1) as ativo')
+                DB::raw('MAX(IF(acessors.id IS NULL, 0, 1)) as ativo')
             )
+            ->groupBy('permissaors.id', 'permissaors.nome', 'permissaors.nome_exibicao')
             ->orderBy('permissaors.nome', 'ASC')
-            ->get();
-
-        return $permissoes;
+            ->get();         
     }
+
     static function permissoesPorAcesso($acessor_id) {
-        $permissoes = DB::table('permissaors')
+        return DB::table('permissaors')
             ->leftJoin('acessor_permissaor', 'permissaors.id', '=', 'acessor_permissaor.permissaor_id')
-            //->leftJoin('acessors', 'acessor_permissaor.acessor_id', '=', 'acessors.id')
             ->leftJoin('acessors', function($join) use ($acessor_id) {
                 $join->on('acessors.id', '=', 'acessor_permissaor.acessor_id')
                     ->where('acessor_permissaor.acessor_id', '=', $acessor_id);
-            })            
+            })
             ->select(
                 'permissaors.id',
                 'permissaors.nome',
                 'permissaors.nome_exibicao',
-                DB::raw('IF(acessors.id IS NULL, 0, 1) as ativo')
+                DB::raw('MAX(IF(acessors.id IS NULL, 0, 1)) as ativo')
             )
+            ->groupBy('permissaors.id', 'permissaors.nome', 'permissaors.nome_exibicao')
             ->orderBy('permissaors.nome', 'ASC')
-            ->get();
-
-        return $permissoes;
+            ->get();         
     }
+ 
 }
